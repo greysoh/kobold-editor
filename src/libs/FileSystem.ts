4ef5f82320b89d5614d1ac339de3437a38389b4c
greysoh <dev@hofers.cloud>
2024-01-12 19:02:44 -0500

    chore: Implements reading.

--- src/libs/FileSystem.ts
+++ src/libs/FileSystem.ts
@@ -1,6 +1,12 @@
 import localForage from "localforage";
 
 // TODO: change to interface? I should learn the difference sometime
+type FileSystemDataBlock = {
+  type: "file",
+  path: string,
+  data: Uint8Array
+}
+
 type FileSystemNode = {
   type: "file" | "folder",
   data?: Uint8Array,
@@ -22,15 +28,24 @@ type FileSystemStruct = {
 export class FileSystem {
   private fileSystemName: string;
   private localWorkingCopy: FileSystemStruct;
+  private localFileSystemEntries: FileSystemDataBlock[];
 
   public hasInitialized: boolean;
 
   constructor(fileSystemName: string) {
+    this.localFileSystemEntries = [];
+
     this.fileSystemName = fileSystemName;
     this.hasInitialized = false;
 
     this.localWorkingCopy = {
-      nodes: [],
+      nodes: [
+        {
+          type: "folder",
+          path: "/"
+        }
+      ],
+
       version: 0,
       
       telemetry: {
@@ -56,9 +71,34 @@ export class FileSystem {
     this.hasInitialized = true;
   }
 
-  // @ts-ignore
   async read(fileName: string): Promise<Uint8Array> {
+    const entry: FileSystemNode | undefined = this.localWorkingCopy.nodes.find((i: FileSystemNode) => i.path == fileName);
+    if (!entry) throw new Error("Entry not found");
+
+    if (entry.type == "folder") throw new Error("Not a file!");
+
+    // Attempt to do a cache hit first
+    const attemptedCacheHit: FileSystemDataBlock | undefined = this.localFileSystemEntries.find((i) => i.path == fileName);
     
+    if (attemptedCacheHit) {
+      // Success!
+      return attemptedCacheHit.data;
+    } else {
+      // Fine, we do a lookup in the real DB...
+      const localData: Uint8Array | null = await localForage.getItem(this.fileSystemName + "_" + fileName);
+      
+      if (!localData) throw new Error("WTF? File should exist in true storage, but doesn't!");
+      else if (!(localData instanceof Uint8Array)) throw new Error("WTF? File on disk is incorrect data type");
+
+      // Now, let's put this in the bag
+      this.localFileSystemEntries.push({
+        type: "file",
+        path: fileName,
+        data: localData
+      });
+
+      return localData;
+    }
   }
 
   // @ts-ignore
@@ -73,6 +113,13 @@ export class FileSystem {
 
   // @ts-ignore
   async ls(dirName: string): Promise<string[]> {
+
+  }
+
+  /**
+   * This syncs the 
+   */
+  async syncToDisk(): Promise<void> {
 
   }
 }
