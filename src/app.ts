cead56aa5819d7884083c0eb93956455d675111c
greysoh <me@greysoh.dev>
2024-01-13 20:47:15 -0500

    chore: Adds saving support.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
-import { FileSystem } from "./libs/FileSystem";
-import { AutobahnFS } from "./libs/AutobahnFS";
-
 import { WebContainer } from "@webcontainer/api";
 import { FitAddon } from "xterm-addon-fit";
 import { Terminal } from "xterm";
 import { edit } from "ace-builds";
 
-import css from "./index.module.css";
 import 'ace-builds/src-noconflict/theme-dracula';
+
+import { FileSystem } from "./libs/FileSystem";
+import { AutobahnFS } from "./libs/AutobahnFS";
+
+import css from "./index.module.css";
 
 console.log("INFO: Initializing core...");
 
@@ -18,6 +19,9 @@ const sidebar: HTMLElement | null = document.getElementById("sidebar");
 
 const editorContainer: HTMLElement | null = document.getElementById("editor");
 const termContainer: HTMLElement | null = document.getElementById("terminal");
+
+const encoder: TextEncoder = new TextEncoder();
+const decoder: TextDecoder = new TextDecoder();
 
 if (!sidebar) throw new Error("Sidebar not found!");
 
@@ -35,6 +39,7 @@ const editor = edit("editor", {
 
 const term = new Terminal();
 const fitAddon = new FitAddon();
+
 term.loadAddon(fitAddon);
 term.open(document.getElementById("terminal") as HTMLElement); // TODO: Nuh uh
 fitAddon.fit();
@@ -43,6 +48,10 @@ editor.setAutoScrollEditorIntoView(true);
 const fs = new FileSystem("koboldfs");
 
 async function main() {
+  // We hit proper initialization!
+  let activeFile: string = "";
+  let currentSaveTimerState: number = 0;
+
   await fs.init();
   await fs.mkdir("/projects");
 
@@ -62,26 +71,29 @@ async function main() {
       }
     }
 
-    window.location.href += "?project=" + projectName; // TODO: can be buggy?
+    window.location.href += "?project=" + projectName; // FIXME: can be buggy?
     return;
   }
 
   const project: string = params.get("project") as string; // Already checked it
-
   if (!(await fs.exists("/projects/" + project, "folder"))) window.location.replace("/");
+
+  // Boot our container, then synchronize the fs
 
   const webcontainerInstance = await WebContainer.boot({
     workdirName: "projects"
   });
   
-  term.write("Synchronizing file system... ");
+  term.write("Synchronizing file system...");
   
   const autoFS: AutobahnFS = new AutobahnFS(fs, webcontainerInstance.fs);
   await autoFS.sync();
 
   await webcontainerInstance.fs.writeFile(".jshrc.sh", `cd "/home/projects/${project}/"\njsh`);
 
-  term.write("[done]\r\n");
+  term.write(" [done]\r\n");
+
+  // Configure our terminal
 
   const shellProcess = await webcontainerInstance.spawn("jsh", ["/home/projects/.jshrc.sh"]);
   const input = shellProcess.input.getWriter();
@@ -97,6 +109,28 @@ async function main() {
   term.onData((data) => {
     input.write(data);
   });
+
+  // Configure the text editor
+
+  function saveTimer(): void {
+    if (currentSaveTimerState != 0) currentSaveTimerState -= 100;
+    if (currentSaveTimerState < 0) currentSaveTimerState = 0;
+    
+    setTimeout(saveTimer, 100);
+  }
+  
+  editor.on("change", async() => {
+    currentSaveTimerState = 2000;
+
+    if (activeFile && currentSaveTimerState == 0) {
+      console.log("Saving changes...");
+      
+      const text: string = editor.getValue();
+      await autoFS.write(activeFile, encoder.encode(text));
+    }
+  });
+
+  saveTimer();
 }
 
 main();
